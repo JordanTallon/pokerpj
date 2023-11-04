2570b103f2be49a8f74ad57365738a02cfef9c54
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2023-11-04 19:23:54 +0000

    create and import style.css

--- src/main/java/ie/dcu/computing/ca314/g10/pokerproject/HelloApplication.java
+++ src/main/java/ie/dcu/computing/ca314/g10/pokerproject/HelloApplication.java
@@ -6,12 +6,16 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
+
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/css/style.css")).toExternalForm());
+
         stage.setTitle("G10 Poker");
         stage.setResizable(false);
         stage.setScene(scene);
