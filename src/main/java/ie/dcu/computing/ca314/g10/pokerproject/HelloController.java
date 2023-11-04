4f676c972b7811fddd71381d11ae88675bdd6b2d
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2023-11-04 19:18:59 +0000

    add menu buttons with name input prompt

--- src/main/java/ie/dcu/computing/ca314/g10/pokerproject/HelloController.java
+++ src/main/java/ie/dcu/computing/ca314/g10/pokerproject/HelloController.java
@@ -2,6 +2,9 @@ package ie.dcu.computing.ca314.g10.pokerproject;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextInputDialog;
+
+import java.util.Optional;
 
 public class HelloController {
     @FXML
@@ -10,5 +13,30 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    protected void onJoinGameClick(){
+        TextInputDialog dialog = new TextInputDialog("");
+        dialog.setTitle("Enter Name");
+        dialog.setHeaderText(null);
+        dialog.setContentText("Please enter your name:");
+
+
+        Optional<String> result = dialog.showAndWait();
+
+        if(result.isPresent()){
+            String name = result.get().trim();
+
+            // Prompt for name input once again if the user failed to give a name.
+            if(name.isEmpty())
+            {
+                onJoinGameClick();
+                return;
+            }
+
+            // Set player name
+            System.out.println("Your name: " + name);
+        }
     }
 }
