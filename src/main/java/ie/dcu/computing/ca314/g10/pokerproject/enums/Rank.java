8abff9b5ccee5a465f33d528e7e6d046314ea902
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2023-11-04 14:08:18 +0000

    add integer associations to Rank enum

--- src/main/java/ie/dcu/computing/ca314/g10/pokerproject/enums/Rank.java
+++ src/main/java/ie/dcu/computing/ca314/g10/pokerproject/enums/Rank.java
@@ -1,21 +1,35 @@
 package ie.dcu.computing.ca314.g10.pokerproject.enums;
 
 /**
- * Represents the possible ranks for a
- playing card.
+ * Represents the possible ranks for a playing card.
  */
 public enum Rank {
-    ACE,
-    KING,
-    QUEEN,
-    JACK,
-    TEN,
-    NINE,
-    EIGHT,
-    SEVEN,
-    SIX,
-    FIVE,
-    FOUR,
-    THREE,
-    TWO
+    ACE(14),
+    KING(13),
+    QUEEN(12),
+    JACK(11),
+    TEN(10),
+    NINE(9),
+    EIGHT(8),
+    SEVEN(7),
+    SIX(6),
+    FIVE(5),
+    FOUR(4),
+    THREE(3),
+    TWO(2);
+
+    private final int value;
+
+    Rank(int value) {
+        this.value = value;
+    }
+
+    /**
+     * Gets the numeric value of the rank.
+     *
+     * @return The card rank as an integer.
+     */
+    public int getValue() {
+        return value;
+    }
 }
