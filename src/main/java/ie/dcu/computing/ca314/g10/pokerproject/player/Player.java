5b3321ea2484957bc077f5687cc511384f0b196f
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2023-11-08 19:52:23 +0000

    add player UI, refactor Player

--- src/main/java/ie/dcu/computing/ca314/g10/pokerproject/player/Player.java
+++ src/main/java/ie/dcu/computing/ca314/g10/pokerproject/player/Player.java
@@ -3,6 +3,9 @@ package ie.dcu.computing.ca314.g10.pokerproject.player;
 import ie.dcu.computing.ca314.g10.pokerproject.core.Hand;
 import ie.dcu.computing.ca314.g10.pokerproject.interfaces.Playable;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 
@@ -13,29 +16,71 @@ public class Player implements Playable {
     private double xPosition;
     private double yPosition;
 
-    /** Player pane, contains all player CSS */
+    /** Player pane, parent to all player styling */
     public StackPane container;
+
+    /** UI Labels used in the player pane */
+    private Label nameLabel;
+    private Label chipLabel;
+    private Label actionLabel;
 
     /** The name displayed for the player. */
     private String displayName;
     /** The total amount of chips the player has. */
-    public int chipsAmount;
+    public int chipCount;
     /** List of individual chip objects the player has. */
     public List <Chip> chips;
     /** The hand of cards assigned to the player. */
     public Hand hand;
 
     public Player (String displayName, int startingChips, int x, int y){
-        this.displayName = displayName;
-        this.chipsAmount = startingChips;
+        // Create player UI
         buildContainer();
+        addContainerLabels();
+
+        // Set player's position (where they are on the board)
         setPosition(x, y);
+
+        // Update player information
+        setDisplayName(displayName);
+        setChipCount(startingChips);
+    }
+
+
+    private void setChipCount(int newChipCount) {
+        this.chipCount = newChipCount;
+        this.chipLabel.setText("" + newChipCount);
+    }
+
+    private void setDisplayName(String displayName) {
+        this.displayName = displayName;
+        this.nameLabel.setText(displayName.toUpperCase());
     }
 
     /** Create the player container */
-    public void buildContainer() {
+    private void buildContainer() {
         this.container = new StackPane();
         this.container.getStyleClass().add("player-container");
+    }
+
+    /** Adds the Player UI labels to the Player container */
+    private void addContainerLabels() {
+        Label nameLabel = new Label("PLAYER NAME");
+
+        this.nameLabel = createLabel("PLAYER NAME", "player-name", Pos.TOP_CENTER, new Insets(33,0,0,0));
+        this.chipLabel = createLabel("CHIP TOTAL", "player-total", Pos.TOP_CENTER, new Insets(48,0,0,0));
+        this.actionLabel = createLabel("ACTION", "player-action", Pos.BOTTOM_CENTER, new Insets(0,0,32,0));
+
+        this.container.getChildren().addAll(this.nameLabel, this.chipLabel, this.actionLabel);
+    }
+
+    /** Helper when creating labels to avoid repetition */
+    private Label createLabel(String text, String styleClass, Pos position, Insets margin) {
+        Label label = new Label(text);
+        label.getStyleClass().add(styleClass);
+        StackPane.setAlignment(label, position);
+        StackPane.setMargin(label, margin);
+        return label;
     }
 
     /**  Method to update the position if needed. (maybe when a player joins or leaves) */
