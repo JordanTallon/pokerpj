971259c11c2dd15c999ebb354767b42ccfc1de79
Jordan Tallon <jordan.tallon3@mail.dcu.ie>
2023-11-08 17:52:15 +0000

    add starting chip setting, pass starting chips to player on creation

--- src/main/java/ie/dcu/computing/ca314/g10/pokerproject/GameController.java
+++ src/main/java/ie/dcu/computing/ca314/g10/pokerproject/GameController.java
@@ -10,7 +10,13 @@ import java.util.Optional;
 
 public class GameController {
 
+    // Represents the player this client owns
     Player localPlayer;
+
+    // Game settings (perhaps move this elsewhere later?)
+    // How many chips a new player starts with
+    int startingChips = 1000;
+
 
     // Player passes their name from menu prompt to this scene
     public void setPlayerName(String playerName) {
@@ -19,7 +25,9 @@ public class GameController {
 
 
     // Handle this on server side only later?
-    private void addNewPlayer(String playerName){
-        localPlayer = new Player();
+    private void addNewPlayer(String displayName){
+        localPlayer = new Player(displayName, startingChips);
+
+        System.out.println(localPlayer.getDisplayName());
     }
 }
